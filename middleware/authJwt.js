af8bf64260b49f92626ea95f6f355cab1570cb53
poleswararao <pmallela77@gmail.com>
2022-11-30 11:30:37 +0530

    access token data change

--- middleware/authJwt.js
+++ middleware/authJwt.js
@@ -20,7 +20,7 @@ verifyToken = (req, res, next) => {
           message: "Unauthorized!"
         });
       }
-      req.userId = decoded.id;
+      req.user = decoded.user;
       next();
     });
   };
